4feff94f60db1642049769de33b06945d68423b6
lsh <lshqwers@163.com>
2020-08-09 20:25:57 +0800

    11

---
+++ day1/more.ts
@@ -0,0 +1,5 @@
+function greeter(person){
+    return 'zNznsdd'+person
+}
+let user="ewee"
+document.body.innerHTML=greeter(user)
---
+++ js/day1/more.js
@@ -0,0 +1,6 @@
+"use strict";
+function greeter(person) {
+    return 'zNznsdd' + person;
+}
+var user = "ewee";
+document.body.innerHTML = greeter(user);
